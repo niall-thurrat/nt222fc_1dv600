f8b75297a5679af0f1e34b9285c03cd6a33685be
niall-thurrat <amidst_art@hotmail.com>
2019-02-18 16:17:09 +0100

    clear installed and called in Hangman

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,4 +1,5 @@
 
+const clear = require('clear')
 const imageGenerator = require('./imageGenerator')
 const wordBoard = require('./wordBoard')
 const messageGenerator = require('./messageGenerator')
@@ -7,11 +8,12 @@ function PlayGame (test) {
   this.test = test
 
   this.test = function () {
+    clear()
     console.log(imageGenerator.getNewImage('banner'))
     console.log(imageGenerator.getNewImage('hangman-image'))
     console.log(wordBoard.updateBoard('t', 2))
     console.log(messageGenerator.newMessage('new-game'))
-    console.log('MENU will appear here')
+    console.log('MENU will appear here\n\n\n\n\n')
   }
 }
 
